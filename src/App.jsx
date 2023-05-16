268240a54660c9def0fd79dedc56831153241b61
zillly <gil-hell@hotmail.com>
2023-05-15 23:12:34 -0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+
+import './App.css'
+import HelloFuntion from './componente/HelloFuntion';
+
+function App() {
+
+
+  return (
+    <>
+      <HelloFuntion
+      nome = "Gilmar"
+      />
+    </>
+  )
+}
+
+export default App
---
+++ src/componente/HelloFuntion.jsx
@@ -0,0 +1,10 @@
+function HelloFuntion(props){
+   return(
+    <>
+     <h1> Hello {props.nome}</h1>
+    </>
+   );
+  
+}
+
+export default  HelloFuntion;
